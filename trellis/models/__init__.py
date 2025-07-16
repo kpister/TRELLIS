53f97e1a519d3fc009d2e4e5ddfe4bd2f2762130
Kaiser Pister <pisterk@gmail.com>
2025-07-15 19:10:16 -0500

    Update __init__.py

--- trellis/models/__init__.py
+++ trellis/models/__init__.py
@@ -49,19 +49,10 @@ def from_pretrained(path: str, **kwargs):
     import os
     import json
     from safetensors.torch import load_file
-    is_local = os.path.exists(f"{path}.json") and os.path.exists(f"{path}.safetensors")
 
-    if is_local:
-        config_file = f"{path}.json"
-        model_file = f"{path}.safetensors"
-    else:
-        from huggingface_hub import hf_hub_download
-        path_parts = path.split('/')
-        repo_id = f'{path_parts[0]}/{path_parts[1]}'
-        model_name = '/'.join(path_parts[2:])
-        config_file = hf_hub_download(repo_id, f"{model_name}.json")
-        model_file = hf_hub_download(repo_id, f"{model_name}.safetensors")
-
+    config_file = f"{path}.json"
+    model_file = f"{path}.safetensors"
+    
     with open(config_file, 'r') as f:
         config = json.load(f)
     model = __getattr__(config['name'])(**config['args'], **kwargs)
